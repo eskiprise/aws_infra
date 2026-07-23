e558806b5dd909d30fcc76122ebf7c6ea54c3254
Viacheslav Kozachok <uberbug544@gmail.com>
2026-07-23 14:05:42 +0300

    Added more

--- lambda/ttrpg_club_notifier/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "lambda_function_name" {
-  value       = module.lambda_function.lambda_function_name
-  sensitive   = false
-  description = "Notifier Lambda function name"
-  depends_on  = [module.lambda_function]
-}
--- ssm/ttrpg_club/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "parameters_arn" {
-  value       = { for p in aws_ssm_parameter.param : p.name => p.arn }
-  sensitive   = false
-  description = "ARN of each SSM parameter, by name"
-  depends_on  = [aws_ssm_parameter.param]
-}
